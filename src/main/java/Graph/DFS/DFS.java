20c84f4ef6384c2b21d0bc3ba1fbd888c4675d12
njxue <ngjxue@gmail.com>
2022-12-08 11:06:42 +0800

    DFS

--- src/main/java/Graph/DFS/DFS.java
+++ src/main/java/Graph/DFS/DFS.java
@@ -1,31 +1,28 @@
-package Graph.BFS;
+package Graph.DFS;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+import java.util.Stack;
 
 import Graph.*;
 
-public class BFS {
-    public static <T> void displayBFS(Graph<T> graph) {
-        Queue<Node<T>> queue = new LinkedList<>();
+public class DFS {
+    public static <T> void displayDFS(Graph<T> graph) {
+        Stack<Node<T>> stack = new Stack<>();
         HashSet<Node<T>> visited = new HashSet<>();
         List<Node<T>> nodes = graph.getVertices();
 
         for (Node<T> src : nodes) {
             if (!visited.contains(src)) {
-                queue.add(src);
-                while (!queue.isEmpty()) {
-                    Node<T> node = queue.poll();
+                stack.push(src);
+                while (!stack.isEmpty()) {
+                    Node<T> node = stack.pop();
                     if (!visited.contains(node)) {
                         visited.add(node);
                         System.out.print(node + " > ");
                         List<Node<T>> neighbours = graph.getNeighbours(node);
                         for (Node<T> nb : neighbours) {
-                            if (!visited.contains(nb)) {
-                                queue.add(nb);
-                            }
+                            stack.push(nb);
                         }
                     }
                 }
--- src/main/java/Graph/Main.java
+++ src/main/java/Graph/Main.java
@@ -1,21 +1,29 @@
 package Graph;
 
 import Graph.BFS.BFS;
+import Graph.DFS.DFS;
 
 public class Main {
     public static void main(String[] args) {
         Graph<Integer> graph = new Graph<Integer>();
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
-        graph.addEdge(1, 3);
         graph.addEdge(0, 3);
-        graph.addEdge(2, 3);
+        graph.addEdge(0, 7);
+        graph.addEdge(1, 4);
+        graph.addEdge(1, 5);
+        graph.addEdge(2, 5);
+        graph.addEdge(2, 7);
+        graph.addEdge(3, 6);
+        graph.addEdge(4, 7);
+        graph.addEdge(5, 6);
 
-        graph.add(4);
-        graph.add(5);
-        graph.addEdge(4,6);
+        graph.addEdge(8, 9);
+
+        graph.add(10);
+
         //graph.displayGraph();
-
         BFS.displayBFS(graph);
+        DFS.displayDFS(graph);
     }
 }
