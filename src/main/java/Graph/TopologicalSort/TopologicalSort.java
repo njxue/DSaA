57fd4819e888e8f9d90a394d82925f952ed956f1
njxue <ngjxue@gmail.com>
2022-12-10 10:53:09 +0800

    DFS toposort

--- src/main/java/Graph/TopologicalSort/TopologicalSort.java
+++ src/main/java/Graph/TopologicalSort/TopologicalSort.java
@@ -34,29 +34,4 @@ public class TopologicalSort {
         }
         return sorted;
     }
-
-    public static <T> List<T> dfs(DirectedGraph<T> graph) {
-        List<T> sorted = new ArrayList<>();
-        List<Node<T>> nodes = graph.getVertices();
-        Stack<Node<T>> stack = new Stack<>();
-        HashSet<Node<T>> visited = new HashSet<>();
-
-        for (Node<T> src : nodes) {
-            if (!visited.contains(src)) {
-                stack.push(src);
-                while (!stack.isEmpty()) {
-                    Node<T> node = stack.pop();
-                    if (!visited.contains(node)) {
-                        visited.add(node);
-                        sorted.add(node.getValue());
-                        List<Node<T>> neighbours = graph.getNeighbours(node);
-                        for (Node<T> nb : neighbours) {
-                            stack.push(nb);
-                        }
-                    }
-                }
-            }
-        }
-        return sorted;
-    }
 }
